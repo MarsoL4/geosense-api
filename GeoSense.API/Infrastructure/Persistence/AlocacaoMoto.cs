abdd56e96cd51ed2a26529710320c948b3da6df9
Enzo Marsola <enzogmarsola@gmail.com>
2025-05-17 20:56:31 -0300

    Adiciona as edntidades AlocacaoMoto e Defeito

---
+++ GeoSense.API/Infrastructure/Persistence/AlocacaoMoto.cs
@@ -0,0 +1,12 @@
+﻿namespace GeoSense.API.Infrastructure.Persistence
+{
+    public class AlocacaoMoto
+    {
+        public long Id { get; set; }
+        public Guid MotoId { get; set; }
+        public DateTime Entrada { get; set; }
+        public DateTime? Saida { get; set; }
+        public Moto Moto { get; set; }
+    }
+
+}
---
+++ GeoSense.API/Infrastructure/Persistence/Defeito.cs
@@ -0,0 +1,10 @@
+﻿namespace GeoSense.API.Infrastructure.Persistence
+{
+    public class Defeito
+    {
+        public long Id { get; set; }
+        public string Descricao { get; set; }
+        public Guid MotoId { get; set; }
+        public Moto Moto { get; set; }
+    }
+}
