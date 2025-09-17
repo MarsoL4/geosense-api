ea01306c4de381138e7a507c0652897a21d55820
MarsoL4 <enzogmarsola@gmail.com>
2025-09-17 08:53:21 -0300

    feat(examples): adiciona classes de exemplos de DTO

---
+++ GeoSense.API/Examples/PatioDTOExample.cs
@@ -0,0 +1,16 @@
+﻿using GeoSense.API.DTOs;
+using Swashbuckle.AspNetCore.Filters;
+
+namespace GeoSense.API.Examples
+{
+    public class PatioDTOExample : IExamplesProvider<PatioDTO>
+    {
+        public PatioDTO GetExamples()
+        {
+            return new PatioDTO
+            {
+                Id = 1
+            };
+        }
+    }
+}
