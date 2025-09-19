350983d466520e2c089330fd267a524722a4c386
Enzo Marsola <enzogmarsola@gmail.com>
2025-09-18 22:03:28 -0300

    Adiciona nova Migration com restrições únicas à tabela MOTO

--- GeoSense.API/Migrations/20250919005939_AddUniqueConstraintsToMoto.Designer.cs
+++ GeoSense.API/Migrations/20250919005939_AddUniqueConstraintsToMoto.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using GeoSense.API.Infrastructure.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Oracle.EntityFrameworkCore.Metadata;
 
@@ -11,9 +12,11 @@ using Oracle.EntityFrameworkCore.Metadata;
 namespace GeoSense.API.Migrations
 {
     [DbContext(typeof(GeoSenseContext))]
-    partial class GeoSenseContextModelSnapshot : ModelSnapshot
+    [Migration("20250919005939_AddUniqueConstraintsToMoto")]
+    partial class AddUniqueConstraintsToMoto
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
