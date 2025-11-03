e14fd8ee1e26d509f7a0ed7cd1dbeff40ba5ee3d
Enzo Marsola <enzogmarsola@gmail.com>
2025-11-03 20:51:11 -0300

    fix(program): remove a referência ao ml service removido do projeto

--- GeoSense.API/Program.cs
+++ GeoSense.API/Program.cs
@@ -39,8 +39,6 @@ namespace GeoSense.API
 
             builder.Services.AddAutoMapper(typeof(MappingProfile));
 
-            builder.Services.AddSingleton<MlPredictionService>();
-
             var connectionString = builder.Configuration.GetConnectionString("Oracle");
             builder.Services.AddDbContext<GeoSenseContext>(options =>
                 options.UseOracle(connectionString));
